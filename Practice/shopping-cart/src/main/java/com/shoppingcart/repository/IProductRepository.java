fe5ff1ce853dfdb44b95f29640c7e866fd02e6ff
Compass <ndphong165@gmail.com>
2022-10-26 16:43:41 +0700

    done th2

---
+++ Practice/shopping-cart/src/main/java/com/shoppingcart/repository/IProductRepository.java
@@ -0,0 +1,9 @@
+package com.shoppingcart.repository;
+
+import com.shoppingcart.model.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductRepository extends PagingAndSortingRepository<Product, Long> {
+}
